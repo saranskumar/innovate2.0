17a61130e1f362e19907e8f5deac8b042a7abeb4
Amith Biju <amithbiju1@gmail.com>
2024-12-23 00:32:14 +0530

    update_fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -141,9 +141,9 @@ export const Navbar = () => {
                 setMobileMenuOpen(false);
               }}
             >
-              <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-700">
+              {/* <button className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-100 hover:bg-gray-700">
                 Dhyuthi 5.0
-              </button>
+              </button> */}
             </Link>
             <button
               type="button"
@@ -156,7 +156,7 @@ export const Navbar = () => {
           </div>
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-700">
-              <div className="space-y-2 py-6">
+              <div className="space-y-2 py-6 pt-11">
                 <Link
                   activeClass="active"
                   to="page1"
